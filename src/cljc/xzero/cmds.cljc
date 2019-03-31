59b0faba3e57bd93f3b572877006183018c404da
Xuelin Wang <xuelin.wang@gmail.com>
2019-03-31 10:39:38 -0700

    script working

--- src/cljc/xzero/cmds.cljc
+++ src/cljc/xzero/cmds.cljc
@@ -3,7 +3,6 @@
 (def cmds
   {"bash" ["/bin/bash" "-c"]
    "clojure" nil
-   "python3" ["/Users/xuelinwang/anaconda3/bin/python3" "-c"]
    })
 
 (def cmd-types (keys cmds))
--- src/cljs/xzero/events.cljs
+++ src/cljs/xzero/events.cljs
@@ -64,3 +64,55 @@
   :initialize-db
   (fn [db _]
     db/default-db))
+
+(rf/reg-event-db
+ :update-value
+ []
+ (fn [db [_ value-path val]]
+   (assoc-in db value-path val)))
+
+(rf/reg-event-db
+ :update-values
+ []
+ (fn [db [_ path-values]]
+   (reduce (fn [db [path val]] (assoc-in db path val)) db (partition 2 path-values))))
+
+
+(defn url-encode [ss] (js/encodeURIComponent ss))
+(defn url-encode-map [mm]
+  (let [nvs (into [] mm)
+        encoded-nvs (map #(into [] (map url-encode %))
+                         nvs)]
+    (into {} (into [] encoded-nvs))))
+
+(defn clj->json [ds] (.stringify js/JSON (clj->js ds)))
+(defn clj->url-encoded-json [ds] (url-encode (clj->json ds)))
+
+(defn process-response [db [_ path response]]
+  (-> db
+      (assoc-in (conj path :loading?) false)
+      (assoc-in (conj path :result) response)))
+
+(rf/reg-event-db
+  :process-cmd-response
+  []
+  (fn [db [_ response]] (process-response db [_ [:cmd] response])))
+
+(rf/reg-event-fx
+ :execute-cmd
+ []
+ (fn [{:keys [db]} [_]]
+   (let
+     [cmd (:cmd db)
+      cmd-type (or (:cmd-type cmd) "bash")
+      script (or (get (:script cmd) cmd-type) "")
+      params {"cmd-type" cmd-type "script" script}]
+     {:http-xhrio {:method          :post
+                   :uri             "/xzeros/cmd/execute"
+                   :format          (ajax/json-request-format)
+                   :params          params
+                   :response-format (ajax/json-response-format {:keywords? false})
+                   :on-success      [:process-cmd-response]
+                   :on-failure      [:process-cmd-response]}
+      :db  (assoc-in db [:cmd :loading?] true)})))
+
