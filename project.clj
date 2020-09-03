f81776917741a664a90396eb10154687c34ffeac
xuelin <xuelin.wang@gmail.com>
2020-09-03 16:09:27 -0700

    bump versions

--- project.clj
+++ project.clj
@@ -5,44 +5,44 @@
 
   :dependencies [[baking-soda "0.2.0" :exclusions [cljsjs/react-bootstrap]]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [cljs-ajax "0.8.0"]
-                 [cljsjs/d3 "5.7.0-0"]
-                 [rid3 "0.2.1"]
-                 [cljsjs/react-popper "0.10.4-0"]
-                 [cljsjs/react-transition-group "2.4.0-0"]
+                 [cljs-ajax "0.8.1"]
+                 [cljsjs/d3 "5.12.0-0"]
+                 [rid3 "0.2.2"]
+                 [cljsjs/react-popper "1.0.2-0"]
+                 [cljsjs/react-transition-group "4.3.0-0"]
                  [clojure.java-time "0.3.2"]
-                 [com.cognitect/transit-clj "0.8.313"]
-                 [com.fasterxml.jackson.core/jackson-core "2.9.7"]
-                 [com.fasterxml.jackson.datatype/jackson-datatype-jdk8 "2.9.7"]
-                 [cprop "0.1.13"]
-                 [day8.re-frame/http-fx "0.1.6"]
-                 [funcool/struct "1.3.0"]
-                 [markdown-clj "1.0.5"]
-                 [metosin/muuntaja "0.6.1"]
-                 [metosin/reitit "0.2.9"]
+                 [com.cognitect/transit-clj "1.0.324"]
+                 [com.fasterxml.jackson.core/jackson-core "2.11.2"]
+                 [com.fasterxml.jackson.datatype/jackson-datatype-jdk8 "2.11.2"]
+                 [cprop "0.1.17"]
+                 [day8.re-frame/http-fx "	0.2.1"]
+                 [funcool/struct "1.4.0"]
+                 [markdown-clj "1.10.5"]
+                 [metosin/muuntaja "0.6.7"]
+                 [metosin/reitit "0.5.5"]
                  [metosin/ring-http-response "0.9.1"]
-                 [mount "0.1.14"]
-                 [nrepl "0.4.5"]
-                 [org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.439" :scope "provided"]
-                 [org.clojure/tools.cli "0.4.1"]
-                 [org.clojure/tools.logging "0.4.1"]
+                 [mount "0.1.16"]
+                 [nrepl "0.8.0"]
+                 [org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.773" :scope "provided"]
+                 [org.clojure/tools.cli "1.0.194"]
+                 [org.clojure/tools.logging "1.1.0"]
                  [org.webjars.bower/tether "1.4.4"]
-                 [org.webjars/bootstrap "4.1.3"]
-                 [org.webjars/font-awesome "5.5.0"]
-                 [org.webjars/webjars-locator "0.34"]
-                 [re-frame "0.10.6"]
-                 [reagent "0.8.1"]
+                 [org.webjars/bootstrap "4.5.2"]
+                 [org.webjars/font-awesome "5.14.0"]
+                 [org.webjars/webjars-locator "0.40"]
+                 [re-frame "1.1.1"]
+                 [reagent "0.10.0"]
                  [ring-webjars "0.2.0"]
-                 [ring/ring-core "1.7.1"]
+                 [ring/ring-core "1.8.1"]
                  [ring/ring-defaults "0.3.2"]
                  [derekchiang/ring-proxy "1.0.2"]
-                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-jetty-adapter "1.8.1"]
                  [secretary "1.2.3"]
-                 [selmer "1.12.5"]]
+                 [selmer "1.12.28"]]
 
   :min-lein-version "2.0.0"
-  
+
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
   :test-paths ["test/clj"]
   :resource-paths ["resources" "target/cljsbuild"]
@@ -59,7 +59,7 @@
    :css-dirs ["resources/public/css"]
    :nrepl-middleware
    [cider/wrap-cljs-repl cider.piggieback/wrap-cljs-repl]}
-  
+
 
   :profiles
   {:uberjar {:omit-source true
@@ -78,8 +78,8 @@
                  :closure-warnings
                  {:externs-validation :off :non-standard-jsdoc :off}
                  :externs ["react/externs/react.js"]}}}}
-             
-             
+
+
              :aot :all
              :uberjar-name "xzero.jar"
              :source-paths ["env/prod/clj"]
@@ -89,19 +89,19 @@
    :test          [:project/dev :project/test :profiles/test]
 
    :project/dev  {:jvm-opts ["-Dconf=dev-config.edn"]
-                  :dependencies [[binaryage/devtools "0.9.10"]
-                                 [cider/piggieback "0.3.10"]
+                  :dependencies [[binaryage/devtools "1.0.2"]
+                                 [cider/piggieback "0.5.1"]
                                  [doo "0.1.11"]
-                                 [expound "0.7.1"]
-                                 [figwheel-sidecar "0.5.17"]
-                                 [pjstadig/humane-test-output "0.9.0"]
-                                 [prone "1.6.1"]
-                                 [re-frisk "0.5.4"]
-                                 [ring/ring-devel "1.7.1"]
-                                 [ring/ring-mock "0.3.2"]]
-                  :plugins      [[com.jakemccrary/lein-test-refresh "0.23.0"]
+                                 [expound "0.8.5"]
+                                 [figwheel-sidecar "0.5.20"]
+                                 [pjstadig/humane-test-output "0.10.0"]
+                                 [prone "2020-01-17"]
+                                 [re-frisk "1.3.4"]
+                                 [ring/ring-devel "1.8.1"]
+                                 [ring/ring-mock "0.4.0"]]
+                  :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]
                                  [lein-doo "0.1.11"]
-                                 [lein-figwheel "0.5.17"]]
+                                 [lein-figwheel "0.5.20"]]
                   :cljsbuild
                   {:builds
                    {:app
@@ -117,9 +117,9 @@
                       :pretty-print true
                       :closure-defines {"re_frame.trace.trace_enabled_QMARK_" true}
                       :preloads [re-frisk.preload]}}}}
-                  
-                  
-                  
+
+
+
                   :doo {:build "test"}
                   :source-paths ["env/dev/clj"]
                   :resource-paths ["env/dev/resources"]
@@ -137,7 +137,7 @@
                       :main "xzero.doo-runner"
                       :optimizations :whitespace
                       :pretty-print true}}}}
-                  
+
                   }
    :profiles/dev {}
    :profiles/test {}})
